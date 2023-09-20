394dd5d651e83c057386393f5d340e00d7531e48
* air <134613063+Jakob-CC@users.noreply.github.com>
2023-09-20 23:17:44 +0200

    steps

---
+++ ffmpegfolders.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Loop through all folders
+for dir in {1..8}; do
+  # Check if folder exists
+  if [ -d "${dir}" ]; then
+    # Change to folder
+    cd "${dir}"
+    
+    # Run ffmpeg command
+    ffmpeg -r 4 -i '4p-000%02d.jpg' -vf "scale=-2:1080,format=yuv420p" -c:v libx264 -r 4 slideshow.mp4
+    
+    # Log message
+    echo "Processed folder ${dir}."
+    
+    # Change back to root directory
+    cd ..
+  fi
+done
---
+++ renamefolders.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Loop through all folders
+for dir in {1..8}; do
+  # Check if folder exists
+  if [ -d "${dir}" ]; then
+    # Change to folder
+    cd "${dir}"
+    
+    # Initialize counter
+    counter=1
+    
+    # Loop through files in folder and rename
+    for file in $(ls 4p-*.jpg | sort -n); do
+      new_name=$(printf "4p-%05d.jpg" ${counter})
+      mv "${file}" "${new_name}"
+      echo "Renamed ${file} to ${new_name}"
+      
+      # Increment counter
+      ((counter++))
+    done
+    
+    # Change back to root directory
+    cd ..
+  fi
+done
+
